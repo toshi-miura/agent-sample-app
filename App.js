8919eb988d4e468c7ee3588ac455f9b2c55b56c8
toshi_miura <toshihiko.miura@gmail.com>
2017-05-30 14:17:00 +0900

    webの管理用の口を追加？（IP縛り等を想定）

--- App.js
+++ App.js
@@ -20,34 +20,55 @@ console.log("get config form env.");
 
 //TODO cosole.log delete!!  password!!!!
 
+const botManageObj = {}
+botManageObj['onlineBotCount'] = 0;
+
 if (botAccountId && bot1userName && botpassword) {
 
     console.log(botAccountId + ":" + bot1userName + ":" + botpassword);
 
     const agent = new agentBot(botAccountId, bot1userName, botpassword);
     agent.start();
+    botManageObj['onlineBotCount']++
 }
 if (botAccountId && bot2userName && botpassword) {
     const agent2 = new agentBot(botAccountId, bot2userName, botpassword);
     console.log(botAccountId + ":" + bot2userName + ":" + botpassword);
+
     // agent2 is bad bot user.
     // agent2.start();
+    //onlineBotCount++;
 }
 if (botAccountId && bot3userName && botpassword) {
     const agent3 = new agentBot(botAccountId, bot3userName, botpassword);
     console.log(botAccountId + ":" + bot3userName + ":" + botpassword);
     agent3.start();
+    botManageObj['onlineBotCount']++
 }
 
 if (botAccountId && bot4userName && botpassword) {
     const agent4 = new agentBot(botAccountId, bot4userName, botpassword);
     console.log(botAccountId + ":" + bot4userName + ":" + botpassword);
     agent4.start();
+    botManageObj['onlineBotCount']++
 }
 
 
 console.log("bot online!!");
 
+
+//server.js
+var http = require('http'); 
+var server = http.createServer();
+
+
+server.on('request', function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    res.write(JSON.stringify(botManageObj) );
+    res.end();
+})
+server.listen(80, '127.0.0.1')
 
 
 
